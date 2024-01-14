bca5fafdef41b307c869c8c800c5b335cd1f3a52
Ziad Emad <115287958+1Ziad-Etman1@users.noreply.github.com>
2024-01-14 20:46:45 +0200

    .

--- Standard_Huffman/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.standard_huffman {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.standard_huffman to javafx.fxml;
-    exports com.example.standard_huffman;
-}
